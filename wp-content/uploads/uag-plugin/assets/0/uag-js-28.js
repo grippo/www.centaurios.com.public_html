98bd0ba720caa4d2e31fbee44e5356e7ae5c6c31
Jorge Grippo <jorge.grippo@gmail.com>
2026-06-24 02:05:56 -0300

    new

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-28.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-c94a9d10', {"layout":"number","heading":"Years of Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-368adb1a', {"layout":"number","heading":"Satisfied Customer","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":98,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-9d06e675', {"layout":"number","heading":"Vertical Markets","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":40,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-d5e79842', {"layout":"number","heading":"Happy Customers","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":500,"totalNumber":500,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-78594fe2' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+ });
